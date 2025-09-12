88774a92fcac353c616c2ec8ee51402f691f835b
btom7447 <tombenjamin7447@gmail.com>
2025-09-12 13:28:53 +0100

    installed swagger

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,6 +3,7 @@ const mongodb = require('../data/database');
 
 // Get all contacts
 const getAll = async (req, res) => {
+    //#swagger.tags=['Contacts]
     const result = await mongodb.getDatabase().db().collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
@@ -12,6 +13,7 @@ const getAll = async (req, res) => {
 
 // Get a single contact by ID
 const getSingle = async (req, res) => {
+    //#swagger.tags=['Contacts]
     const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contacts').findOne({ _id: contactId });
     res.setHeader('Content-Type', 'application/json');
@@ -20,6 +22,7 @@ const getSingle = async (req, res) => {
 
 // Create a new contact
 const createContact = async (req, res) => {
+    //#swagger.tags=['Contacts]
     const contact = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -37,6 +40,7 @@ const createContact = async (req, res) => {
 
 // Update a contact
 const updateContact = async (req, res) => {
+    //#swagger.tags=['Contacts]
     const contactId = new ObjectId(req.params.id);
     const contact = {
         firstName: req.body.firstName,
@@ -45,7 +49,7 @@ const updateContact = async (req, res) => {
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday,
     };
-    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: contactId }, contact);
+    const response = await mongodb.getDatabase().db().collection('contacts').updateOne({ _id: contactId }, { set: contact });
     if (response.modifiedCount > 0) {
         res.status(204).send();
     } else {
@@ -55,6 +59,7 @@ const updateContact = async (req, res) => {
 
 // Delete a contact
 const deleteContact = async (req, res) => {
+    //#swagger.tags=['Contacts]
     const contactId = new ObjectId(req.params.id);
     const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: contactId });
     if (response.deletedCount > 0) {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
@@ -7,8 +8,20 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON
 app.use(express.json());
 
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
+
 // Routes
 app.use('/', require('./routes'));
+app.use('/', require('./routes/swagger'))
 
 // Connect DB + Start server
 mongodb.initDb((err) => {
